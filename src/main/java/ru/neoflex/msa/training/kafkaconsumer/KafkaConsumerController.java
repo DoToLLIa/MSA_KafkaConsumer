17ed8797be53ee40c270e264ddf971f1e7ba8df4
Anton Talanov <dotollia@mail.ru>
2021-01-20 12:32:56 +0400

    change receive to String

--- src/main/java/ru/neoflex/msa/training/kafkaconsumer/KafkaConsumerController.java
+++ src/main/java/ru/neoflex/msa/training/kafkaconsumer/KafkaConsumerController.java
@@ -2,12 +2,11 @@ package ru.neoflex.msa.training.kafkaconsumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-import ru.neoflex.msa.training.kafkaconsumer.BankAccount;
 
 @Component
 public class KafkaConsumerController {
     @KafkaListener(topics = "bankAccountTopic")
-    public void msgListener(BankAccount bankAccount) {
+    public void msgListener(String bankAccount) {
         System.out.println(bankAccount);
     }
 }
